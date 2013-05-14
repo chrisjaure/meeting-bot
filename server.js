335f943a97f85de3332091dd17add14011cd40e3
Chris Jaure <chrisjaure@gmail.com>
2013-05-14 14:33:26 +0000

    add blinking led

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var app = express();
 var env = new nunjucks.Environment(new nunjucks.FileSystemLoader(__dirname));
 var index = env.getTemplate('index.html');
 var isInAMeeting = false;
+var blinkInt;
+var gpio = process.env.GPIO || '38';
 
 var play = function (file) {
 
@@ -27,9 +29,23 @@ var play = function (file) {
 
 }
 
+var blink = function() {
+	try {
+		fs.writeFileSync('/sys/class/gpio/gpio'+gpio+'/direction', 'high');
+		setTimeout(function(){
+			fs.writeFileSync('/sys/class/gpio/gpio'+gpio+'/direction', 'low');
+		}, 1000);
+	} catch(e) {}
+}
+
 var playMeetingStart = play.bind(this, './meeting-start.wav');
 var playMeetingEnd = play.bind(this, './meeting-end.wav');
 var playIntro = play.bind(this, './meeting-bot.wav');
+
+// export the gpio
+try {
+	fs.writeFileSync('/sys/class/gpio/export', gpio);
+} catch (e) {}
 
 app.get('/', function(req, res) {
 	res.charset = 'utf-8';
@@ -41,12 +57,14 @@ app.get('/meeting/start', function(req, res) {
 	isInAMeeting = true;
 	res.send('Dad is in a meeting now.');
 	playMeetingStart();
+	blinkInt = setInterval(blink, 2000);
 });
 
 app.get(/\/meeting\/(end|stop)/, function(req, res) {
 	isInAMeeting = false;
 	res.send('Dad is done with his meeting.');
 	playMeetingEnd();
+	clearInterval(blinkInt);
 });
 
 app.listen(80, function(err){
